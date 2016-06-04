5eb5f459e515e7daa1ad041460f6cf471d76dc2f
Critical Mass Sternfahrt <criticalmasssternfahrt@users.noreply.github.com>
2016-06-04 02:07:42 +0200

    Updated theme and added more geojson files

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -sSL -o - "https://github.com/criticalmasssternfahrt/theme/releases/download/v0.0.2/v0.0.2-release.tar.gz" | tar -xzv
+curl -sSL -o - "https://github.com/criticalmasssternfahrt/theme/releases/download/v0.0.3/v0.0.3-release.tar.gz" | tar -xzv
