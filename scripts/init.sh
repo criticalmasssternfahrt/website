9dee76b42683a022bdef404b3d6a4922cb170560
Critical Mass Sternfahrt <criticalmasssternfahrt@users.noreply.github.com>
2016-06-07 01:43:56 +0200

    Added index partials

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -sSL -o - "https://github.com/criticalmasssternfahrt/theme/releases/download/v0.0.6/v0.0.6-release.tar.gz" | tar -xzv
+curl -sSL -o - "https://github.com/criticalmasssternfahrt/theme/releases/download/v0.0.7/v0.0.7-release.tar.gz" | tar -xzv
